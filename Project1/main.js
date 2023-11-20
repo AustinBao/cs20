b8d63b60f269dea73126a83a6e1fcb19552132a7
Austin Bao <a.bao1@EDU.epsb.ca>
2023-11-20 09:58:10 -0700

    weatherApi working

--- Project1/main.js
+++ Project1/main.js
@@ -1,10 +1,26 @@
-document.getElementById("submitbtn").addEventListener("click", weather);
+document.getElementById("submitbtn").addEventListener("click", getWeatherData);
 
-function weather() {
+async function getWeatherData() {
+  // Get users city
   let city = document.getElementById("city").value;
+  // ApiKey from my weather data provider (https://openweathermap.org/api)
   let apiKey = "063bdab6bd3f0e17b1afe04736c947ff";
 
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  // Calling Api and return in form of JSON file
+  let response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+  let data = await response.json();
 
-  console.log(url);
+  // Pass returned data to display weather function
+  dislpayWeatherData(data);
+}
+
+function dislpayWeatherData(data) {
+  // iterate the keys and values of each element in data.
+  for ([key, val] of Object.entries(data)) {
+    console.log(key, val);
+  }
+  document.getElementById("weatherIcon").src =
+    "https://openweathermap.org/img/wn/10d@2x.png";
 }
