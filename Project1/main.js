ddc2bec0cb9db16358a656382a5eba7bab7fb3af
Austin Bao <a.bao1@EDU.epsb.ca>
2023-11-21 09:58:51 -0700

    added css

--- Project1/main.js
+++ Project1/main.js
@@ -10,6 +10,7 @@ async function getWeatherData() {
   let response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
   );
+  // waits for response then parses data as a JSON for later use
   let data = await response.json();
 
   // Pass returned data to display weather function
@@ -21,6 +22,24 @@ function dislpayWeatherData(data) {
   for ([key, val] of Object.entries(data)) {
     console.log(key, val);
   }
-  document.getElementById("weatherIcon").src =
-    "https://openweathermap.org/img/wn/10d@2x.png";
+
+  // Coord
+
+  // Weather
+
+  let icon = data.weather[0].icon;
+  console.log(icon);
+  document.getElementById(
+    "weatherIcon"
+  ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  let main = data.weather[0].main;
+  document.body.style.backgroundImage = `url(backgrounds/${main}.png)`;
+  // Main
+
+  // Wind
+
+  // Sys (Country)
+
+  // name
 }
